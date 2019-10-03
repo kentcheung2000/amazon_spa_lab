891d56fe234857136845cfe6430dc3875b85cf16
kentcheung2000 <kentcheung2000@yahoo.com>
2019-10-03 10:51:28 -0700

    React basic lab 3

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -2,15 +2,19 @@ import React from "react";
 
 
 function ProductDetails(props) {
-    console.log(props.seller.full_name)
+    //console.log("THISIS PRODUCT PROPS INSIDE PRODUCT DETAILS", props);
+    //console.log(props.seller.full_name)
+
     return (
         <div>
             <h1>Product Details</h1>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-            <p>{props.price}</p>
 
-            <p> seller = {props.seller.full_name} created at = {props.created_at}</p>
+            <h2>{props.product.title}</h2>
+            <p>{props.product.description}</p>
+            <p>{props.product.price}</p>
+            {<p> seller: {props.product.seller.full_name} created at: {props.product.created_at}</p>}
+
+
         </div>
 
 
--- src/components/ReviewDetails.js
+++ src/components/ReviewDetails.js
@@ -2,15 +2,29 @@ import React from "react";
 
 
 function ReviewDetails(props) {
+  console.log("All reviews:", props.reviews)
+
+
+
   return (
     <div className="ui segment ReviewDetails">
       <div className="ui header">
-        <h1>Review Details</h1>
+        <h3>Reviews</h3>
       </div>
-      <p>{props.rating}</p>
-      <p>{props.body}</p>
-      <p>{props.created_at}</p>
-      <p>{props.reviewer.full_name}</p>
+
+      {props.reviews.map(review => (
+        <div>
+          <p>{review.rating}</p>
+          <p>{review.body}</p>
+          <p>{review.reviewer.full_name}</p>
+        </div>
+
+      ))}
+
+
+
+
+
     </div>
   );
 }
